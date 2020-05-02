8a0f612fdf8bb4e4f2bd2dfd6e1ea95572b557ae
tantmse61751 <tantmse61751@gmail.com>
2020-05-02 22:28:38 +0700

    assignment - week1

---
+++ assignments/week1/assignment.js
@@ -0,0 +1,43 @@
+const students = [
+  {
+    name: 'Nam',
+    age: 24,
+    gender: 'male',
+  },
+  {
+    name: 'Mai',
+    age: 22,
+    gender: 'female',
+  },
+  {
+    name: 'Trang',
+    age: 23,
+    gender: 'female',
+  },
+  {
+    name: 'An',
+    age: 20,
+    gender: 'male',
+  },
+  {
+    name: 'Thien',
+    age: 27,
+    gender: 'male',
+  },
+];
+
+  // Assignment 3
+  var maleStudentCounting = 0;
+  const gender = 'gender';
+  students.forEach(student => {
+      if(student[gender] === 'male') {
+        console.log(student.name);
+        maleStudentCounting++;
+      }
+  });
+  console.log('number of male student: ' + maleStudentCounting);
+
+  // Assignment 4
+  const studentNames = students.map(student => student.name);
+  console.log(studentNames);
+
