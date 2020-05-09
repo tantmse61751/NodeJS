de2747a9aade636690e97d271fb6b4131137c725
tantmse61751 <tantmse61751@gmail.com>
2020-05-10 00:20:03 +0700

    fix w1 - ass3

--- assignments/week1/assignment.js
+++ assignments/week1/assignment.js
@@ -1,46 +1,66 @@
 const students = [
   {
-    name: 'Nam',
+    name: "Nam",
     age: 24,
-    gender: 'male',
+    gender: "male",
   },
   {
-    name: 'Mai',
+    name: "Mai",
     age: 22,
-    gender: 'female',
+    gender: "female",
   },
   {
-    name: 'Trang',
+    name: "Trang",
     age: 23,
-    gender: 'female',
+    gender: "female",
   },
   {
-    name: 'An',
+    name: "An",
     age: 20,
-    gender: 'male',
+    gender: "male",
   },
   {
-    name: 'Thien',
+    name: "Thien",
     age: 27,
-    gender: 'male',
+    gender: "male",
   },
 ];
 
 // Assignment 3
-  var maleStudentCounting = 0;
-  const gender = 'gender';
-  students.forEach(student => {
-      if(student[gender] === 'male') {
-        console.log(student.name);
-        maleStudentCounting++;
-      }
-  });
-  console.log('number of male student: ' + maleStudentCounting);
+// var maleStudentCounting = 0;
+// const gender = "gender";
+// students.forEach((student) => {
+//   if (student[gender] === "male") {
+//     console.log(student.name);
+//     maleStudentCounting++;
+//   }
+// });
+// console.log("number of male student: " + maleStudentCounting);
 
 // fix Assignment 3
 
+// function callback(value, result) {
+//   const countByType = result[students[i][type]];
+//   result[students[i][type]] = (countByType || 0) + 1;
+// }
 
-  // Assignment 4
-  const studentNames = students.map(student => student.name);
-  console.log(studentNames);
+const reduce = (arr, callback, result = {}) => {
+  for (let i = 0; i < arr.length; i++) {
+    result = callback(i, arr[i], result);
+  }
+  return result;
+};
 
+const countByType = (type) => {
+  return reduce(students, (index, value, result) => {
+    const countType = result[value[type]];
+    result[value[type]] = (countType || 0) + 1;
+    return result; 
+  });
+};
+
+console.log(countByType("gender"));
+
+// Assignment 4
+// const studentNames = students.map((student) => student.name);
+// console.log(studentNames);
