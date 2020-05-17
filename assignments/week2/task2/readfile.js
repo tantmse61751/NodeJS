50785158918484878f46f7a3291dec667531e565
tantmse61751 <tantmse61751@gmail.com>
2020-05-17 23:46:39 +0700

    w2 - ass2

--- assignments/week2/task2/readfile.js
+++ assignments/week2/task2/readfile.js
@@ -1,27 +1,52 @@
-const fs = require('fs');
+const fs = require("fs");
+const formatDistanceStrict = require("date-fns/formatDistanceStrict");
+const vnLocale = require("date-fns/locale/vi");
 
-new Promise((resolve,reject)=>{
-    fs.readFile('products.json',(err, data)=>{
-        if (err) {
-            reject(err); // in the case of error, control flow goes to the catch block with the error occured.
-            console.log(err);
-        }
-        else {
-            resolve(data);  // in the case of success, control flow goes to the then block with the content of the file.
-            let json = JSON.stringify(data);
-            console.log(json.toString);
-        }
-    });
+const p = new Promise((resolve, reject) => {
+  fs.readFile("products.json", (err, data) => {
+    if (err) {
+      reject(err); // in the case of error, control flow goes to the catch block with the error occured.
+      console.log(err);
+    } else {
+      resolve(JSON.parse(data)); // in the case of success, control flow goes to the then block with the content of the file.
+      // console.log(JSON.parse(data));
+    }
+  });
 })
-.catch((err)=>{
-    throw err; //  handle error here.
-});
+  .then((value) => {
+    console.log(`Number of Product: ${value.length}`);
+    value.forEach((p) => {
+      console.log(
+        `${p.id} - ${p.name} - ${
+          formatPriceWithCommas(p.price) + "VND"
+        } - Cập nhật cách đây ${calculateDistanceDate(
+          convertToDate(p.dateUpdated)
+        )}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err); //  handle error here.
+  });
 
+function convertToDate(str) {
+  return new Date(str);
+}
 
+function formatPriceWithCommas(price) {
+  price = price.toString();
+  const pattern = /(-?\d+)(\d{3})/;
+  while (pattern.test(price)) {
+    price = price.replace(pattern, "$1,$2");
+  }
+  return price;
+}
 
-
-
-
+function calculateDistanceDate(dateUpdated) {
+  return formatDistanceStrict(dateUpdated, new Date(), {
+    locale: vnLocale,
+  });
+}
 
 // let products = new Array;
 // function readContent(products) {
